a5f10dd3c3c15826bf2b06d29aa20b73e15fed90
alonsovalenzuelae <alonsovalenzuela1985@gmail.com>
2025-12-04 10:58:26 -0300

    final updates

--- tests/login.fail.spec.ts
+++ tests/login.fail.spec.ts
@@ -3,5 +3,5 @@ import { test } from '../fixtures/pom.fixture';
 test('Deliberate failure: wrong flash message', async ({ pm }) => {
     await pm.loginPage.openLoginPage();
     await pm.loginPage.userLogin("fakeUser", "fakePass");
-    await pm.securePage.assertSuccess();
+    await pm.loginPage.assertFailedUsername();
 });
